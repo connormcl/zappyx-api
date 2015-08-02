652a9f17d2846456a2c6e34f3af1c9a87410e882
Connor McLaughlin <connor.mclaughlin@yale.edu>
2015-08-02 13:35:34 -0700

    Requires device token on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :device_token, presence: true
 
   has_secure_password
   validates :password, length: { in: 6..30 }, :on => :create
